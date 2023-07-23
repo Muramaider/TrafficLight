c115c7bb3544660ac56af116c6c19fe2dfafc4ea
Alexey <vinogradov.al.kon@gmail.com>
2023-07-23 18:04:35 +0300

    Create logic for switching colors for traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redTrafficLightView: UIView!
+    @IBOutlet var yellowTrafficLightView: UIView!
+    @IBOutlet var greenTrafficLightView: UIView!
+    @IBOutlet var switchTrafficLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
-
+    @IBAction func switchTrafficLightButtonDidTupped() {
+        switchTrafficLightButton.setTitle("NEXT", for: .normal)
+        
+        if redTrafficLightView.alpha == 1 {
+            redTrafficLightView.alpha = 0.3
+            yellowTrafficLightView.alpha = 1
+        } else if yellowTrafficLightView.alpha == 1 {
+            yellowTrafficLightView.alpha = 0.3
+            greenTrafficLightView.alpha = 1
+        } else if greenTrafficLightView.alpha == 1 {
+            greenTrafficLightView.alpha = 0.3
+            redTrafficLightView.alpha = 1
+        } else {
+            redTrafficLightView.alpha = 1
+        }
+    }
+    
+    private func setupUI() {
+        redTrafficLightView.layer.cornerRadius = 62.5
+        yellowTrafficLightView.layer.cornerRadius = 62.5
+        greenTrafficLightView.layer.cornerRadius = 62.5
+        switchTrafficLightButton.layer.cornerRadius = 10
+    }
+    
 }
 
